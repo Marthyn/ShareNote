7ca163e62190272a746d3fd2cfbec70b3e1b3d01
Marthyn Olthof <marthyn@live.nl>
2012-04-11 14:17:45 +0300

    Standard font sizes added

--- CourseWorkCsharp2/WriteNote.cs
+++ CourseWorkCsharp2/WriteNote.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace ShareNote
+namespace CourseWorkCsharp2
 {
     public partial class WriteNote : Form
     {
@@ -18,16 +18,96 @@ namespace ShareNote
 
         private void fontChanged(object sender, EventArgs e)
         {
+            Font oldfont, newfont;
             int i = this.fontBox.SelectedIndex;
-            switch(i)
+            switch (i)
             {
+                //normal
                 case 0:
 
+                    break;
+                //title 1
+                case 1:
+                    oldfont = this.noteText.SelectionFont;
+                    newfont = new Font(oldfont.FontFamily, 20);
+                    this.noteText.SelectionFont = newfont;
                 break;
 
-                case 1:
-                this.noteText.SelectionFont.SizeInPoints = 20;
+                //title 2
+                case 2:
+                    oldfont = this.noteText.SelectionFont;
+                    newfont = new Font(oldfont.FontFamily, 18);
+                    this.noteText.SelectionFont = newfont;
+                break;
+
+                //title 3
+                case 3:
+                    oldfont = this.noteText.SelectionFont;
+                    newfont = new Font(oldfont.FontFamily, 16);
+                    this.noteText.SelectionFont = newfont;
+                break;
+
+                //Paragraph
+            }
 
         }
+
+        private void boldButton_Click(object sender, EventArgs e)
+        {
+            Font oldFont, newFont;
+
+            oldFont = this.noteText.SelectionFont;
+
+            if (oldFont.Bold)
+            {
+                newFont = new Font(oldFont, oldFont.Style & ~FontStyle.Bold);
+            }
+            else
+            {
+                newFont = new Font(oldFont, oldFont.Style | FontStyle.Bold);
+            }
+
+            this.noteText.SelectionFont = newFont;
+            this.noteText.Focus();
+        }
+
+        private void italicButton_Click(object sender, EventArgs e)
+        {
+            Font oldFont, newFont;
+
+            oldFont = this.noteText.SelectionFont;
+
+            if (oldFont.Italic)
+            {
+                newFont = new Font(oldFont, oldFont.Style & ~FontStyle.Italic);
+            }
+            else
+            {
+                newFont = new Font(oldFont, oldFont.Style | FontStyle.Italic);
+            }
+
+            this.noteText.SelectionFont = newFont;
+            this.noteText.Focus();
+        }
+
+        private void buttonUnderline_Click(object sender, EventArgs e)
+        {
+            Font oldFont, newFont;
+
+            oldFont = this.noteText.SelectionFont;
+
+            if (oldFont.Underline)
+            {
+                newFont = new Font(oldFont, oldFont.Style & ~FontStyle.Underline);
+            }
+            else
+            {
+                newFont = new Font(oldFont, oldFont.Style | FontStyle.Underline);
+            }
+
+            this.noteText.SelectionFont = newFont;
+            this.noteText.Focus();
+        }
+
     }
 }
